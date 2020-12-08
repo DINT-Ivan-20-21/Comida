e8336405e8cc9048f75c0c641e9d646e2f49e512
Iván López <ivan.lopezgon@iesdoctorbalmis.com>
2020-12-08 13:07:01 +0100

    Agregar archivos de proyecto.

---
+++ Comida/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Comida
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ObservableCollection<Plato> lista = new ObservableCollection<Plato>(Plato.GetSamples(Environment.CurrentDirectory +"/Imagenes/"));
+            PlatoListBox.ItemsSource = lista;
+            TipoComboBox.ItemsSource = new string[] { "China", "Americana", "Mexicana" };
+        }
+    }
+}
